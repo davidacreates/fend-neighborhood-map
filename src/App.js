b38a143c0608df1889c8e797eed5897c2f26bd78
Davida Pitts <Davida@orangemooncreative.com>
2018-10-09 18:45:57 +0800

    Add google map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import { loadScript } from './helpers';
 import './App.css';
 
 export default class App extends Component {
+  componentDidMount = () => {
+    const baseURL = 'https://maps.googleapis.com/maps/api/js?';
+    const API = 'AIzaSyAYWYxiUDePggeSPxYRnQt-2PCeQ0IJ6lA';
+    // load google maps script asynchronously
+    loadScript(`${baseURL}key=${API}&callback=initMap`);
+    // display the map on the page
+    window.initMap = this.initMap;
+  };
+
+  // initialize google map & assign coordinates
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: -8.4843527, lng: 115.2633453 },
+      zoom: 15,
+    });
+  };
+
   render() {
-    return <div className="App">This is my react app</div>;
+    return (
+      <main>
+        <div id="map" />
+      </main>
+    );
   }
 }
