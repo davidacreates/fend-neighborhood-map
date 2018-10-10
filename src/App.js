3a49a2246fb0fb9c6d5b99daaba43d3324df2a86
Davida Pitts <Davida@orangemooncreative.com>
2018-10-10 12:40:50 +0800

    Add venue markers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { loadGoogleMaps, MAP_BASE_URL } from './helpers';
+import { loadMap, MAP_BASE_URL } from './helpers';
 import {
   MAP_API_KEY,
   FSQUARE_CLIENT_ID,
@@ -9,39 +9,67 @@ import {
 import './App.css';
 
 export default class App extends Component {
-  componentDidMount = () => {
-    // invoke function to get data from foursquare
-    this.getFSquareData();
-    // connect the initMap function to the global window
-    // google maps can now invoke the function
-    window.initMap = this.initMap;
-    // load google maps script asynchronously
-    // pass the callback reference to initMap
-    loadGoogleMaps(`${MAP_BASE_URL}key=${MAP_API_KEY}&callback=initMap`);
+  state = {
+    venues: [],
   };
 
-  // initialize google map & assign coordinates
+  componentDidMount = () => {
+    // invoke function to get data from foursquare
+    this.getVenues();
+  };
+
+  displayMap = () => {
+    // connect the initMap function to the global window
+    // google maps can now invoke the function
+    // load google maps script asynchronously
+    // pass the callback reference to initMap
+    loadMap(`${MAP_BASE_URL}key=${MAP_API_KEY}&callback=initMap`);
+    window.initMap = this.initMap;
+  };
+
+  // initialize google map
+  // assign coordinates
+  // loop through venues array and create a marker for each venue
   initMap = () => {
+    const myLatLng = { lat: -8.5064764, lng: 115.26023 };
+    const { venues } = this.state;
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: -8.4843527, lng: 115.2633453 },
-      zoom: 15,
+      center: myLatLng,
+      zoom: 14,
+    });
+    // FIX: refactor arrow function so no error present
+    venues.map(v => {
+      const marker = new window.google.maps.Marker({
+        position: { lat: v.venue.location.lat, lng: v.venue.location.lng },
+        map,
+        title: v.venue.name,
+      });
     });
   };
 
   // use axios to grab select data from foursquare api
-  getFSquareData = () => {
-    const FSQUARE_BASE_URL = 'https://api.foursquare.com/v2/venues/explor?';
+  // store the retrieved data in the places state
+  // display the map on the page after the venues array is no longer empty
+  getVenues = () => {
+    const FSQUARE_BASE_URL = 'https://api.foursquare.com/v2/venues/explore?';
     const params = {
       client_id: FSQUARE_CLIENT_ID,
       client_secret: FSQUARE_CLIENT_SECRET,
-      section: 'topPicks',
+      section: 'food',
       near: 'Ubud',
       v: 20180910,
     };
 
+    // FIX: make sure error state displays in the browser not just console
+    // TODO: limit the number of markers displayed. use react 16 tut for help.
     axios
       .get(FSQUARE_BASE_URL + new URLSearchParams(params))
-      .then(res => console.log(res.data.response.groups[0].items))
+      .then(res => {
+        this.setState(
+          { venues: res.data.response.groups[0].items },
+          this.displayMap()
+        );
+      })
       .catch(error => console.log(error));
   };
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 export const MAP_BASE_URL = 'https://maps.googleapis.com/maps/api/js?';
 
 // src: https://www.klaasnotfound.com/2016/11/06/making-google-maps-work-with-react/
-export function loadGoogleMaps(url) {
+export function loadMap(url) {
   const ref = window.document.getElementsByTagName('script')[1];
   const script = window.document.createElement('script');
   script.src = url;
