1403090a584b386852ed7f0447c5322471484a76
Davida Pitts <Davida@orangemooncreative.com>
2018-10-12 08:05:45 +0800

    Initial commit for new direction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import React from 'react';
-import Map from './components/Map';
+import React, { Component } from 'react';
 import './App.css';
 
-// // TODO: Remove below commented code if I don't use state in this component
-// export default class App extends Component {
-//   state = {
-//     venues: [],
-//     photos: [],
-//   };
-
-//   render() {
-//     const { venues } = this.state;
-//     return (
-//       <div>
-//         <Map venues={venues} />
-//       </div>
-//     );
-//   }
-// }
-
-const App = () => (
-  <div>
-    <Map />
-  </div>
-);
-
-export default App;
+export default class App extends Component {
+  render() {
+    return <div>Welcome to React</div>;
+  }
+}
--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,7 @@
 import { FSQUARE_CLIENT_ID, FSQUARE_CLIENT_SECRET } from './credentials';
 
 export const MAP_BASE_URL = 'https://maps.googleapis.com/maps/api/js?';
-export const FSQUARE_REC_BASE_URL =
-  'https://api.foursquare.com/v2/venues/explore?';
-export const FSQUARE_PHOTO_BASE_URL = 'https://api.foursquare.com/v2/venues/';
+export const FSQUARE_BASE_URL = 'https://api.foursquare.com/v2/venues/';
 
 export const DEFAULT_CENTER = { lat: -8.5064764, lng: 115.26023 };
 export const DEFAULT_ZOOM = 14;
@@ -14,13 +12,6 @@ export const FSQUARE_VENUE_PARAMS = {
   section: 'food',
   near: 'Ubud',
   limit: 10,
-  v: 20180910,
-};
-export const FSQUARE_PHOTO_PARAMS = {
-  client_id: FSQUARE_CLIENT_ID,
-  client_secret: FSQUARE_CLIENT_SECRET,
-  group: 'venue',
-  limit: 1,
   v: 20180910,
 };
 
